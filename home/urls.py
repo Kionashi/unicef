5bda12a90f6fc68420b9813e085dc0d8476b3c28
AnibalCardozo <anibal@sappitotech.com>
2020-12-03 17:38:59 -0400

    first commit

---
+++ home/urls.py
@@ -0,0 +1,19 @@
+"""Routes"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from .views import home as home_views
+
+urlpatterns = [
+    path('', home_views.home, name='home'),
+    path('what-we-do', home_views.what_we_do, name='what_we_do'),
+    path('research-and-reports', home_views.research_and_reports, name='research_and_reports'),
+    path('stories', home_views.stories, name='stories'),
+    path('take-action', home_views.take_action, name='take_action'),
+    path('press-centre', home_views.press_centre, name='press_centre'),
+    path('about-UNICEF', home_views.about_unicef, name='about_unicef'),
+    path('where-we-work', home_views.where_we_work, name='where_we_work'),
+    path('careers', home_views.careers, name='careers'),
+    path('donate', home_views.donate, name='donate'),
+    path('send-email', home_views.send_email, name='send_email'),
+]
---
+++ home/views/home.py
@@ -0,0 +1,179 @@
+""" Home views. """
+# Django
+from django.conf import settings
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse
+from django.core.mail import EmailMessage, send_mail
+from io import BytesIO
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+import os
+import twocheckout
+
+
+def home(request):
+    """ Load home view """
+    context = {}
+    return render(request, 'en/home.html', context)
+
+def what_we_do(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/what-we-do.html', context)
+
+def research_and_reports(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/research-and-reports.html', context)
+
+def stories(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/stories.html', context)
+
+def take_action(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/take-action.html', context)
+
+def press_centre(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/press-centre.html', context)
+
+def about_unicef(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/about.html', context)
+
+def where_we_work(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/where-we-work.html', context)
+
+def careers(request):
+    """ Load what we do view """
+    context = {}
+    return render(request, 'en/careers.html', context)
+
+def donate(request):
+    """ Load what we do view """
+    # print('===========HERE WE GOOOO===========')
+    # twocheckout.Api.auth_credentials({
+    #     'private_key': 'C45E1E7A-5AC8-4F56-8F52-76878BB9664E',
+    #     'seller_id': '250599272465',
+    #     'mode': 'sandbox'
+    # })
+
+    # params = {
+    #     'merchantOrderId': '123',
+    #     'token': 'ODAxZjUzMDEtOWU0MC00NzA3LWFmMDctYmY1NTQ3MDhmZDFh',
+    #     'currency': 'USD',
+    #     'total': '1.00',
+    #     'billingAddr': {
+    #         'name': 'Testing Tester',
+    #         'addrLine1': '123 Test St',
+    #         'city': 'Columbus',
+    #         'state': 'OH',
+    #         'zipCode': '43123',
+    #         'country': 'USA',
+    #         'email': 'cchristenson@2co.com',
+    #         'phoneNumber': '555-555-5555'
+    #     }
+    # }
+    # try:
+    #     result = twocheckout.Charge.authorize(params)
+    #     print(result.responseCode)
+    # except twocheckout.TwocheckoutError as error:
+    #     print(error.msg)
+    # print('=======================================')
+    context = {}
+    return render(request, 'en/donate.html', context)
+
+def send_email(request):
+    """ Generate pdf to atttach to donation email """
+   
+    context = {
+        'name': 'Anibal :D'
+    }
+    generate_pdf('en/pdfs/donation.html', context)
+    
+    # Send response email after a donation with generated pdf
+    message = 'Hola'
+    subject = 'Hey hey people'
+    email_address = 'anibal@sappitotech.com'
+    email = EmailMessage(subject, message, 'cardozo.anibal@gmail.com', [email_address])
+    email.content_subtype = 'html'
+    file = open('media/test.pdf', 'r')
+    email.attach('test.pdf',file.read(),'text/plain')
+    email.send()
+
+    return HttpResponse('Sent')
+
+def convertHtmlToPdf(self,sourceHtml, outputFilename):
+    """
+        Open output file for writing (truncated binary) and
+        converts HTML code into pdf file format
+
+    :param sourceHtml: The html source to be converted to pdf
+    :param outputFilename: Name of the output file as pdf
+    :return: Error if pdf not generated successfully
+    """
+    resultFile = open(outputFilename, "w+b")
+    # convert HTML to PDF
+    pisaStatus = pisa.CreatePDF(sourceHtml, dest=resultFile)
+    # close output file
+    resultFile.close()
+    # return True on success and False on errors
+    return pisaStatus.err 
+
+def generate_pdf(template_src, context_dict={}):
+    """ Function to render a django template into a pdf in a static location """
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(
+        BytesIO(html.encode("UTF-8")), result, link_callback=link_callback)
+    file = open('media/test.pdf', "w+b")
+    pisa_status = pisa.CreatePDF(html.encode('utf-8'), dest=file, encoding='utf-8')
+    file.close()
+
+    return True
+
+def render_to_pdf(template_src, context_dict={}):
+    """Function to render a django template into a pdf """
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(
+        BytesIO(html.encode("UTF-8")), result, link_callback=link_callback)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    # use short variable names
+    sUrl = settings.STATIC_URL      # Typically /static/
+    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+    mUrl = settings.MEDIA_URL       # Typically /static/media/
+    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+
+    # convert URIs to absolute system paths
+    if uri.startswith(mUrl):
+        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+    elif uri.startswith(sUrl):
+        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+    else:
+        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            'media URI must start with %s or %s' % (sUrl, mUrl)
+        )
+    return path
